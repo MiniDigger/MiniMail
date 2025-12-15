4874b599163069bfca1fa3fa67f7eb926872e30c
MiniDigger | Martin <admin@minidigger.dev>
2025-12-15 10:11:16 +0100

    fix sync folders

--- server/tasks/sync-folders.ts
+++ server/tasks/sync-folders.ts
@@ -1,5 +1,7 @@
 import { useJazzWorker } from "~~/server/services/jazz";
 import { getFolders } from "~~/server/services/mail";
+import { Folder, FolderType, Mail } from "#shared/schema";
+import { co, CoList, getLoadedOrUndefined, z } from "jazz-tools";
 
 export default defineTask({
   meta: {
@@ -20,10 +22,60 @@ export default defineTask({
     worker.root.users.forEach((user) => {
       user.data.accounts.forEach(async (account) => {
         const actualFolders = await getFolders({ user: account.username, pass: account.password });
-        account.folders.$jazz.applyDiff(actualFolders);
+        const existingFolders = (getLoadedOrUndefined(account.folders) || []) as unknown as CoList<FolderType>;
+        const stats = syncFolders(actualFolders, existingFolders);
+        console.log("Synced account", account.username, "of user", user.name, ":", stats);
       });
     });
 
     return { result: "Success" };
   },
 });
+
+function syncFolders(imapFolders: { name: string; path: string[] }[], existingFolders: CoList<FolderType>) {
+  function buildKey(folder: { name: string; path: string[] }) {
+    return folder.path.join("/") + "/" + folder.name;
+  }
+
+  const imapKeys = new Set(imapFolders.map(buildKey));
+  const jazzFolders = new Map<string, any>();
+  for (const folder of existingFolders) {
+    if (!folder) continue;
+    jazzFolders.set(buildKey(folder), folder);
+  }
+
+  const stats = { created: [] as string[], updated: [] as string[], removed: [] as string[] };
+
+  for (const folder of imapFolders) {
+    // Create folders that exist on IMAP but not in Jazz
+    const key = buildKey(folder);
+    if (!jazzFolders.has(key)) {
+      stats.created.push(key);
+      const coFolder = Folder.create(
+        {
+          name: folder.name,
+          path: folder.path,
+          mails: co.list(Mail).create([], { owner: existingFolders.$jazz.owner }),
+          mailIndex: co.record(z.string(), Mail).create({}, { owner: existingFolders.$jazz.owner }),
+        },
+        { owner: existingFolders.$jazz.owner }
+      );
+      existingFolders.$jazz.push(coFolder);
+    } else {
+      // TODO update folder?
+      stats.updated.push(key);
+    }
+  }
+
+  // Remove folders that exist in Jazz but not on IMAP
+  existingFolders.$jazz.remove((folder) => {
+    const key = buildKey(folder);
+    if (!imapKeys.has(key)) {
+      stats.updated.push(key);
+      return true;
+    }
+    return false;
+  });
+
+  return stats;
+}
